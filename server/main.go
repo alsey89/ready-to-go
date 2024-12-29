ca72dab1e0a11a9bb340bd6160586ec10ed69cd5
alsey89 <phyokyawsoe89@gmail.com>
2024-12-29 16:37:46 +0800

    feat(server,client): add basic template

---
+++ server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"funcedup/internal/schema"
+	"funcedup/pkg/config"
+	"funcedup/pkg/logger"
+	"funcedup/pkg/pgconn"
+	"funcedup/pkg/server"
+
+	"go.uber.org/fx"
+)
+
+func init() {
+	//! CONFIG PRECEDENCE: OVERRIDE > ENV > CONFIG FILE > FALLBACK
+
+	// *OPTIONAL* OVERRIDE GLOBAL LOG LEVEL, INTENDED FOR DEVELOPMENT
+	// config.OverrideGlobalLogLevel("debug")
+
+	config.SetUpConfig("SERVER", "yaml", "./")
+}
+
+func main() {
+	app := fx.New(
+		//* Modules ---------------------------------------------------------------
+		logger.InjectModule("logger"),
+		pgconn.InjectModule("database"),
+		server.InjectModule("server"),
+		//* Domains ---------------------------------------------------------------
+
+		//* Migration -------------------------------------------------------------
+		fx.Invoke(func(m *pgconn.Module) {
+			m.ApplySchema(
+				true,
+				schema.User{},
+			)
+		}),
+		//* fx logs ---------------------------------------------------------------
+		fx.NopLogger,
+	)
+	app.Run()
+}
